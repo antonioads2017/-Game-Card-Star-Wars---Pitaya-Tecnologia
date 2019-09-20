df7de0ddfad9aa2d8e5aeef1fd71a577f995013f
antonio <antoniofilhonerde@gmail.com>
2019-09-20 10:57:20 -0300

    iniciando projeto spring

---
+++ src/main/java/com/pitaya/br/starwars/GameCardStarWarsApplication.java
@@ -0,0 +1,13 @@
+package com.pitaya.br.starwars.Game.Card.Star.Wars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCardStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCardStarWarsApplication.class, args);
+	}
+
+}
